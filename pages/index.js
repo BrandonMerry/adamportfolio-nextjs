7c20380580717bab3c50b5790ede0b9e04914304
brandon <brandon.l.merry@gmail.com>
2022-10-08 22:45:02 +0800

    added project items component to destructure

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import About from '../components/About';
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
+import Projects from '../components/Projects';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Projects />
     </div>
   );
 }
