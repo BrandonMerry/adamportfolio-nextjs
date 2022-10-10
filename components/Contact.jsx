eee31d1b454c4d8eb2b3184d221e701ef9dba4ab
brandon <brandon.l.merry@gmail.com>
2022-10-10 12:38:28 +0800

    added a success page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,7 +10,7 @@ import ContactImg from '../public/assets/contact.jpeg';
 const Contact = () => {
   return (
     <div id="contact" className="w-full lg:h-screen">
-      <div className="max-w-[1240px] m-auto px-2 py-16 w-full px-6 ">
+      <div className="max-w-[1240px] m-auto py-16 w-full px-6 ">
         <p className="text-xl tracking-widest uppercase text-[#145f9f]">
           Get In Touch
         </p>
@@ -74,13 +74,17 @@ const Contact = () => {
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
               <form
-                action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060"
+                name="contact"
+                action="/success"
                 method="POST"
-                encType="multipart/form-data"
+                data-netlift="true"
               >
+                <input type="hidden" name="form-name" value="contact" />
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Name</label>
+                    <label htmlFor="name" className="uppercase text-sm py-2">
+                      Name
+                    </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
@@ -88,7 +92,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                    <label htmlFor="phone" className="uppercase text-sm py-2">
                       Phone Number
                     </label>
                     <input
@@ -99,7 +103,9 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Email</label>
+                  <label htmlFor="email" className="uppercase text-sm py-2">
+                    Email
+                  </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="email"
@@ -107,7 +113,9 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
+                  <label htmlFor="subject" className="uppercase text-sm py-2">
+                    Subject
+                  </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
@@ -115,14 +123,19 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
+                  <label htmlFor="message" className="uppercase text-sm py-2">
+                    Message
+                  </label>
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
                     name="message"
                   ></textarea>
                 </div>
-                <button className="w-full p-4 text-gray-100 mt-4 bg-gradient-to-r from-[#145f9f] to-[#145FD6]  text-white">
+                <button
+                  type="submit"
+                  className="w-full p-4 mt-4 bg-gradient-to-r from-[#145f9f] to-[#145FD6]  text-white"
+                >
                   Send Message
                 </button>
               </form>
